d1d354f46f281bbe3903dd94ff6e2a78afb9d677
Wenjie SUN <sunwjie@gmail.com>
2025-02-26 15:17:57 +0100

    misc: add error message when no barcode detected in bc_extract_sc_fastq()

--- R/function-single_cell_seq.R
+++ R/function-single_cell_seq.R
@@ -285,6 +285,11 @@ bc_extract_sc_fastq <- function(
     )
     rownames(metadata) <- names(messyBc)
 
+    # check if no barcodes found
+    if (all(sapply(messyBc, nrow) == 0)) {
+        message("No barcode found. Please check the input file and pattern.")
+    }
+
     output <- BarcodeObj(metadata = metadata, messyBc = messyBc)
 
     output
