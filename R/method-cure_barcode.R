0fd25b3bd20d350192689d46061fd34cc826bcbe
Wenjie SUN <sunwjie@gmail.com>
2025-02-28 22:18:23 +0100

    [doc] clean the NEWS

--- R/method-cure_barcode.R
+++ R/method-cure_barcode.R
@@ -151,8 +151,6 @@ setMethod("bc_cure_cluster", c("BarcodeObj"), function(
             }
         )
 
-        print(cleanBc)
-
         ## The correction log
         #     cleanProc <- lapply(correct_out,
         #       function(d) {
